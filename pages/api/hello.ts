57e359fe157c883bea1ed6b9be0b2bbe43edf48e
user1 <code.softwaredevg@gmail.com>
2023-08-14 21:05:40 +0100

    Commit v3

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,4 +1,4 @@
-/* // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
@@ -11,4 +11,3 @@ export default function handler(
 ) {
   res.status(200).json({ name: 'John Doe' })
 }
- */
